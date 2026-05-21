1dd3a76a8770ebd22a5113cf1e4fa9ed9a5f5b48
Hugo Cabanes <hugo33tab@gmail.com>
2026-05-21 12:28:30 +0200

    Set

--- numpy_ufunc/exercice11.py
+++ numpy_ufunc/exercice11.py
@@ -4,3 +4,19 @@ import numpy as np
 x = np.sinh(np.pi/2)#we can use sinh() cosh() tanh()
 
 print(x)
+
+arr = np.array([np.pi/2, np.pi/3, np.pi/4, np.pi/5])
+
+x1 = np.cosh(arr)
+
+print(x1)
+
+x2 = np.arcsinh(1.0)# we can also use hyperbolic on arcsin() arccos() and arctan()
+
+print(x2)
+
+arr1 = np.array([0.1, 0.2, 0.5])
+
+x3 = np.arctanh(arr1)
+
+print(x3)
---
+++ numpy_ufunc/exercice12.py
@@ -0,0 +1,7 @@
+import numpy as np
+#This exercice is about set used for operation using intersections, union and difference operations 
+arr = np.array([1, 1, 1, 2, 3, 4, 5, 5, 6, 7])
+
+x = np.unique(arr)
+
+print(x)
