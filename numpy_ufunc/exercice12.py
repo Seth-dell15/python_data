7e39f2c6ac679e34c5fd843ced05eb872d4ce56e
Hugo Cabanes <hugo33tab@gmail.com>
2026-05-21 12:42:46 +0200

    Set

--- numpy_ufunc/exercice12.py
+++ numpy_ufunc/exercice12.py
@@ -1,7 +1,36 @@
 import numpy as np
 #This exercice is about set used for operation using intersections, union and difference operations 
+
 arr = np.array([1, 1, 1, 2, 3, 4, 5, 5, 6, 7])
 
-x = np.unique(arr)
+x = np.unique(arr)#we create a set
 
 print(x)
+
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([3, 4, 5, 6])
+
+newarr = np.union1d(arr1, arr2)# union
+
+print(newarr)
+
+arr3 = np.array([1, 2, 3, 4])
+arr4 = np.array([3, 4, 5, 6])
+
+newarr1 = np.intersect1d(arr3, arr4, assume_unique=True)#intersection
+
+print(newarr1)
+
+set1 = np.array([1, 2, 3, 4])
+set2 = np.array([3, 4, 5, 6])
+
+newarr2 = np.setdiff1d(set1, set2, assume_unique=True)#found the difference in the first set
+
+print(newarr2)
+
+set3 = np.array([1, 2, 3, 4])
+set4 = np.array([3, 4, 5, 6])
+
+newarr3 = np.setxor1d(set3, set4, assume_unique=True)#found the difference between the two set
+
+print(newarr3)
